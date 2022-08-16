8e6cf3e4f8692e5358f4d56383f40ae1dd9f6935
deepika-37 <107699081+deepika-37@users.noreply.github.com>
2022-08-16 20:25:29 +0530

    Add files via upload

---
+++ check_leap.js
@@ -0,0 +1,18 @@
+var Check_Leap = (year) => 
+{
+ if(year%4 === 0) {
+    
+    if(year%400 !== 0 && year%100 === 0) {
+      return "Non Leap Year";
+    }
+    
+    return "Leap Year";
+  }
+    
+  
+  return "Non Leap Year";
+
+  
+
+};
+ 
---
+++ reverse_number.js
@@ -0,0 +1,8 @@
+var Reverse_Number = (N) => 
+{
+  let n = String(N).split("");
+  
+  return parseInt(n.reverse().join(""));
+
+};
+ 
